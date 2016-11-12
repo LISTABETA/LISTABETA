bab53209d09471bd56565efc06475530727cd4d4
Patrick Müller <patrick.e.muller@gmail.com>
2016-11-12 02:01:55 -0200

    Adds rescue NoMethodError to recreate versions migration

--- db/migrate/20161112004515_recreate_versions_of_startups_screenshot.rb
+++ db/migrate/20161112004515_recreate_versions_of_startups_screenshot.rb
@@ -2,8 +2,11 @@ class Startup < ActiveRecord::Base;end
 
 class RecreateVersionsOfStartupsScreenshot < ActiveRecord::Migration
   def change
-    Startup.find_each do |startup|
-      startup.screenshot.recreate_versions! if startup.screenshot.present?
+    begin
+      Startup.find_each do |startup|
+        startup.screenshot.recreate_versions! if startup.screenshot.present?
+      end
+    rescue NoMethodError
     end
   end
 end
