aab3af9e6259d12dfed38f44726bab2f8711fd97
Gabriel Carpenedo <cbgabe@gmail.com>
2016-11-21 20:47:55 -0200

    Creates about page

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,9 @@ Listabeta::Application.routes.draw do
 
   root 'pages#home'
 
-  get 'dashboard', to: 'pages#dashboard', as: :dashboard
-  get 'mercados/(:tag)', to: 'pages#markets', as: :markets
+  get 'dashboard',        to: 'pages#dashboard',  as: :dashboard
+  get 'sobre',            to: 'pages#about',      as: :about
+  get 'mercados/(:tag)',  to: 'pages#markets',    as: :markets
 
   resources :startups do
     get 'autocomplete_startup_markets', on: :collection
